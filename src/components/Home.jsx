ea3e4b9fcc20565fc123e22ff10daa4d53778861
Makenson Noel <Makenson.noel@yahoo.com>
2022-08-10 13:28:03 +0000

    12th commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,11 +14,11 @@ const Home = () => {
           I'm a Software Engineer.
         </h2>
         <p className='text-[#3d3d3d] py-4 max-w-[700px]'>
-          I Developed this website using Firebase, Express.js, React, Node.js and Tailwind CSS.
+          I developed this website using Firebase, Express.js, React, Node.js and Tailwind CSS.
         </p>
         <div>
           <button className='text-[#0a192f] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#858585] hover:border-[#ffffff]'>
-            There's More
+            There's more
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
             </span>
