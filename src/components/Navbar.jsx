35c4e8f31c245034ab5fd44b3fd3d8e4fe9c5523
Makenson Noel <Makenson.noel@yahoo.com>
2022-08-10 14:23:24 +0000

    14th commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed z-10 w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed z-10 w-full h-[64px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
         <img src={Logo} alt='Logo Image' style={{ width: '50px' }} />
       </div>
