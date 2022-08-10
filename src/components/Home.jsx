1cbae2f3d975c3e8e374543f4850197731a3f7be
Makenson Noel <Makenson.noel@yahoo.com>
2022-08-10 13:18:31 +0000

    12th commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
   return (
-    <div name='home' className='w-full bg-[#ffffff]'>
+    <div name='home' className='w-full h-screen bg-[#ffffff]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-[#3d3d3d]'>Hi, my name is</p>
