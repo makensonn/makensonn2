1d78fdf14e153d24fef809fbcd3f716280413011
Makenson Noel <Makenson.noel@yahoo.com>
2022-08-10 14:28:08 +0000

    15th commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <div className='sm:text-right text-4xl font-bold text-[#0a192f]'>
-              <p>Hello, I'm Makenson, nice to meet you. Please take a look around.</p>
+              <p>Hi, thank you for visiting.</p>
             </div>
             <div>
               <p>I enjoy writing code and have been developing software for several years with over 700+ contributions to open source code on GitHub. 
